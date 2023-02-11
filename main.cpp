3dbabcf2fe5e223f11e9ff99ac08223387849098
Ajanthan05 <t.rajah05@gmail.com>
2023-02-11 12:39:25 +0530

    add submodule pull down

--- main.cpp
+++ main.cpp
@@ -6,11 +6,40 @@ using namespace std;
 //target_include_diredtories(${PROJECT_NAME} PUBLI Adder) // after
 #include <adder.h>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
 	// prints hello world
 	cout << "Hello World\n";
 	cout<< add(9.5f, 7.2f) <<endl;
+
+	GLFWwindow* window;
+
+	if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+	window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+	while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        //draw();
+
+        // Update animation
+        //animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
 	return 0;
 }
